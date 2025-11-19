444c9fcd27c45b423c354c27aab4238460be5a9d
paulbgtr <pbg.work@pm.me>
2025-11-19 20:43:41 +0200

    basic oled setup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
+import machine
 
-while True:
-    print("hello from micropython")
-    time.sleep(1)
+import ssd1306
+
+i2c = machine.I2C(0, scl=machine.Pin(5), sda=machine.Pin(4))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+oled.fill(0)
+oled.text("driver works", 0, 0)
+oled.show()
